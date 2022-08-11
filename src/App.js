f0295b0c4d8a76adf609580699cb571c415e90eb
wxcocoxw <xluxaeternax@protonmail.com>
2022-08-10 23:26:11 -0400

    Basic Implementation of Pera Wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {PeraWalletConnect} from "@perawallet/connect";
+import axios from 'axios';
+import Button from '@mui/material/Button';
+
+// Create the PeraWalletConnect instance outside of the component
+const peraWallet = new PeraWalletConnect();
 
 function App() {
+
+  const [accountAddress, setAccountAddress] = useState(null);
+  const isConnectedToPeraWallet = !!accountAddress;
+
+  useEffect(() => {
+    // Reconnect to the session when the component is mounted
+    peraWallet.reconnectSession().then((accounts) => {
+      // Setup the disconnect event listener
+      peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
+
+      if (accounts.length) {
+        setAccountAddress(accounts[0]);
+      }
+    });
+  }, []);
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let url = 'https://algoindexer.algoexplorerapi.io/v2/accounts/TMHP2YNFRTS4NGY3IPNP7KWYYWF3S3ZUFUWQJJDMYF72THHCPJGCMDGJGI'
+        const response = await axios.get(url);
+        const responseFormatted = await response.data;
+        const responseAccount = responseFormatted['account'];
+        const amountFormatted = (responseAccount.amount/1000000);
+        console.log(amountFormatted)
+        
+        setData(amountFormatted);
+        setError(null);
+
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Button variant="outlined"
+        onClick={
+          isConnectedToPeraWallet ? handleDisconnectWalletClick : handleConnectWalletClick
+        }>
+        {isConnectedToPeraWallet ? "Disconnect" : "Connect to Pera Wallet"}
+      </Button>
+
+      <div>
+        <h2>{isConnectedToPeraWallet ? `Your Wallet Address is ${accountAddress}` : "Connect First, dummy."}</h2>
+        <h3>Algo Balance:</h3>
+
+        {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+
+      {data && (
+        <div>{data}</div>
+      )}
+     
+      
+      </div>
+    </main>
   );
+
+  function handleConnectWalletClick() {
+    peraWallet
+      .connect()
+      .then((newAccounts) => {
+        // Setup the disconnect event listener
+        peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
+
+        setAccountAddress(newAccounts[0]);
+      })
+      .reject((error) => {
+        // You MUST handle the reject because once the user closes the modal, peraWallet.connect() promise will be rejected.
+        // For the async/await syntax you MUST use try/catch
+        if (error?.data?.type !== "CONNECT_MODAL_CLOSED") {
+          // log the necessary errors
+        }
+      });
+  }
+
+  function handleDisconnectWalletClick() {
+    peraWallet.disconnect();
+
+    setAccountAddress(null);
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {Helmet} from 'react-helmet';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#ffffff',
+        darker: '#053e85',
+      },
+      neutral: {
+        main: '#64748B',
+        contrastText: '#fff',
+      },
+    },
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Helmet>
+        <title>Pera Connect - Example</title>
+        <meta name="author" content="coco" />
+      </Helmet>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
